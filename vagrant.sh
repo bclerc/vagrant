04865277d6b624c7c5d73d9b335da9667ab2fdc8
Bryce CLERC <bclerc@e1r13p13.42.fr>
2019-01-26 18:59:35 +0100

    yep

--- vagrant.sh
+++ vagrant.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+ORANGE='\033[0;33m'
 VAGRANT="."
 read -p "Inserez le chemin du dossier d'installation  : " VAGRANT
 mkdir $VAGRANT/vagrant
 $VAGRANT/vagrant
 curl https://raw.githubusercontent.com/bclerc/vagrant/master/Vagrantfile > $VAGRANT/vagrant/Vagrantfile
+echo "${ORANGE}Instalation de vagrant-vbguest ... "
 vagrant plugin install vagrant-vbguest
-read -p "Vagrant installer. Voulez vous lancez valgrant ? [y/n] " UP
+echo "${GREEN}Voulez-vous lancer valgrant ? [y/n] "
+read UP
 if [ $UP = "y" ]
     then 
-        echo "Lancement de vagrant ... " && vagrant up;
+        echo "${ORANGE}Lancement de vagrant ... " && vagrant up && echo "${GREEN}Vagrant en ligne !";
     else
         exit 0;
 fi
